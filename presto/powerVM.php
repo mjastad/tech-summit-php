39d2c1ea37d49a213e11f5bbdb1ea2f291359f8b
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:45:06 -0700

    Update powerVM.php

--- presto/powerVM.php
+++ presto/powerVM.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Class VMPower: Container class defining JSON body for VM power state change
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/VMPower.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
+
 class VMPower {
 
    private $obj = null;
