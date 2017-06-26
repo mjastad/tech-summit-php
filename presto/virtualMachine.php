16affe5db440b02b0883d4a1cd4c2e72b8d5dcb8
Michael Jastad <mjastad@netapp.com>
2017-06-26 14:00:13 -0700

    Update virtualMachine.php

--- presto/virtualMachine.php
+++ presto/virtualMachine.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * Class VM: Class VirtualMachine: A non-mutable container object designed around NTNX REST
+ * endpiint VMS resource.
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/virtualMachine.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
+
 class VM {
 
    private $instance = null;
