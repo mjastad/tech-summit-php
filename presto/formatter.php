713d461a20e8f2981e3f9ea88350d6eee5270e6a
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:32:45 -0700

    Update formatter.php

--- presto/formatter.php
+++ presto/formatter.php
@@ -3,8 +3,11 @@
 /**
  * Formats a JSON body
  *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
  * @category   Core
- * @package    Formatter.php
+ * @package    presto/Formatter.php
  * @author     M.Jastad <michael.jastad@nutanix.com>
  * @copyright  2017 Nutanix, Inc.
  * @license    N/A
