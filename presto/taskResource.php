b951419629c888337c1ce003a4e051623dc5f90f
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:55:53 -0700

    Update taskResource.php

--- presto/taskResource.php
+++ presto/taskResource.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Class TaskResource: Service class opersting on behalf of a task reference or handle.
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/taskResource.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
+
 require_once('resource.php');
 require_once('task.php');
 
