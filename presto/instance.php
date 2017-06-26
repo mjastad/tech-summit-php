66071ad139d34b6d9fea30037b1269e952ec1d92
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:38:25 -0700

    Update instance.php

--- presto/instance.php
+++ presto/instance.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Class instance: Parent class for resource
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/instance.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    N/A
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated Class deprecated in Release 1.0.1
+ */
+
+
 class Instance {
 
   private $INSTANCE = null;
