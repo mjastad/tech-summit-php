af42d88cc7a25ea6d0d9b87fd986c816e0c79c56
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:43:52 -0700

    Update op.php

--- presto/op.php
+++ presto/op.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Class Op: Container class defining REST operations
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/op.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
 class Op {
 
    const POST = "POST";
