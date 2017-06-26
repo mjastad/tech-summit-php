17c7e3f294346b0a4a3cd0ae641e664d7e3436ac
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:21:23 -0700

    Update formatter.php

--- presto/formatter.php
+++ presto/formatter.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Formats a JSON body
+ *
+ * @category   Core
+ * @package    Formatter.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    N/A
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ */
+
+
 class Formatter extends RecursiveIteratorIterator {
 
     public function current()
