86528211e8cb6a4a30ac50e2d272a9abd4a8ce48
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:41:37 -0700

    Update main.php

--- presto/main.php
+++ presto/main.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * File Main: NTNX REST API Driver.
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/main.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
 //imports
 require_once('virtualMachineResource.php');
 require_once('storageContainerResource.php');
