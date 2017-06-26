3d34ebcad44a4e9f31c4398a6cf300c0824e0869
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:56:55 -0700

    Update user.php

--- presto/user.php
+++ presto/user.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Class User: A non-mutable object designed to represent a user of a host RESTful endpoint.
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/user.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
 class User {
 
   private $username = "";
