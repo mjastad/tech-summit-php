049015a472ba7b68d33d93cb367b70be8a1918b2
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:57:42 -0700

    Update utilities.php

--- presto/utilities.php
+++ presto/utilities.php
@@ -1,5 +1,20 @@
 <?php
 
+/**
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header must
+ * remain intact.
+ *
+ * @category   Core
+ * @package    presto/utilities.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated Class available since Release 1.0.0
+ */
+
+
 function getIterator($json) {
    $jsonIterator = new RecursiveIteratorIterator(
 	new RecursiveArrayIterator(json_decode($json, TRUE)),
