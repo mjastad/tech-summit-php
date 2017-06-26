84377b6d9ce06ddf52e9fe5ff4e27bbc8f0ed556
Michael Jastad <mjastad@netapp.com>
2017-06-26 13:50:53 -0700

    Update storageContainerResource.php

--- presto/storageContainerResource.php
+++ presto/storageContainerResource.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Class StorageContainerService: A service pattern object designed around NTNX StorageContainer resource RESTful operations.
+ *
+ * LICENSE: This source file is use-as-is and is subject to copyright(c).  Header MUST NOT be removed.
+ *
+ * @category   Core
+ * @package    presto/storageContainerResource.php
+ * @author     M.Jastad <michael.jastad@nutanix.com>
+ * @copyright  2017 Nutanix, Inc.
+ * @license    USE-AS-IS
+ * @version    Release: @1.0.1
+ * @since      Class available since Release 1.0.0
+ * @deprecated N/A
+ */
+
+
 require_once('resource.php');
 require_once('storageContainer.php');
 
